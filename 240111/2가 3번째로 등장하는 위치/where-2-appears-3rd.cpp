6a236a1b28bdfaa153dd43edb2d7ded6c6a9553b
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2024-01-11 01:21:36 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, a, cnt = 0;
+    cin >> n;
+    for (int i = 0 ; i < n; i++){
+        cin >> a;
+        if (a == 2)
+            cnt++;
+        
+        if (cnt == 3){
+            cout << i+1;
+            return 0;
+        }
+    }
+}
