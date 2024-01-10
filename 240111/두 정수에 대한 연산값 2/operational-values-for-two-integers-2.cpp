243b1526ed152cdbb90a891ebdf8f9aa0fc1a368
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2024-01-11 08:55:11 +0900

    Title: 두 정수에 대한 연산값 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240111/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+
+    if (n > m){
+        n *= 2;
+        m += 10;
+    }
+    else{
+        n += 10;
+        m *= 2;
+    }
+
+    cout << n << " " << m
+}
