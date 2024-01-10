ac36755169d18e2be66b2042c68f0eba1370e364
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2024-01-11 08:38:03 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 240111/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 int main() {
     int n, m;
+    int Narr[101];
+    int Marr[101];
+
     cin >> n >> m;
 
-    for (int i = 1; i <= m; i++){
-        for (int j = 1; j <= n; j++){
-            if (m * i == n * j){
-                cout << m * i;
+    if (n > m) {
+        int tmp = m;
+        m = n;
+        n = tmp;
+    }
+
+    for (int i = 0; i < n; i++) {
+        Narr[i] = n * (i + 1);
+    }
+    for (int i = 0; i < m; i++) {
+        Marr[i] = m * (i + 1);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (Narr[i] == Marr[j]) {
+                cout << Narr[i];
                 return 0;
             }
         }
