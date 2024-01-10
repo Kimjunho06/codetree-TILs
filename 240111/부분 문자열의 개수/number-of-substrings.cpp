4ea2c29493f7a24385fb9992df8774046a31b83f
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2024-01-11 03:32:57 +0900

    Title: 부분 문자열의 개수, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240111/부분 문자열의 개수/number-of-substrings.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int cnt = 0;
+    string str1, str2;
+    stack<char> st;
+    bool check = false;
+
+    cin >> str1 >> str2;
+
+    for (int i = 0; i < str1.size(); i++){
+        st.push(str1[i]);
+    }
+
+    while(!st.empty()){
+        if (st.top() == str2[0]){
+            check = true;
+            st.pop();
+        }
+        else{
+            st.pop();
+        }
+
+        if (check){
+            if (st.top() == str2[1]){
+                cnt++;
+            }
+            check = false;
+        }
+    }
+
+    cout << cnt;
+}
