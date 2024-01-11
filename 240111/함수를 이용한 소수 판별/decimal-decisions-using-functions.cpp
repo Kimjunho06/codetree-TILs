ca7d25a9f3868b87aaa7c29418643bc1c75450a2
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2024-01-11 09:08:08 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int n){
+    for (int i = 2; i < n; i++){
+        if (n % i == 0) return 0; 
+    }
+    return n;
+}
+
+int main() {
+    int n, m, sum = 0;
+    cin >> n >> m;
+
+    for (int i = n; i <= m; i++){
+        sum += f(i);
+    }
+
+    cout << sum;
+}
