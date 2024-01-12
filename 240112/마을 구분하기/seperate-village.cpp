c37e48c575b0d01bfe18c0a70111adcffa11ecbb
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2024-01-12 10:25:57 +0900

    Title: 마을 구분하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240112/마을 구분하기/seperate-village.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<vector<bool>> visited;
+vector<vector<int>> board;
+vector<int> answer;
+int dx[4] = { 0, 0, 1, -1 };
+int dy[4] = { 1, -1, 0, 0  };
+
+int cnt;
+
+int DFS(int x, int y) {
+
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx < 0 || nx > board[0].size() - 1) continue;
+		if (ny < 0 || ny > board.size() - 1) continue;
+
+		if (!visited[ny][nx] && board[ny][nx] == 1) {
+			visited[ny][nx] = true;
+			cnt++;
+			DFS(nx, ny);
+		}
+	}
+
+	return cnt;
+}
+
+int main() {
+	int n;
+
+	cin >> n;
+	visited.resize(n);
+	board.resize(n);
+
+	for (int i = 0; i < n; i++) {
+		board[i].resize(n);
+		visited[i].resize(n);
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	int house = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (visited[i][j] == false && board[i][j] == 1)
+			{
+				house++;
+				cnt = 0;
+				answer.push_back(DFS(j, i));
+			}
+		}
+	}
+	sort(answer.begin(), answer.end());
+
+	cout << house << "\n";
+	for (auto a : answer) {
+		cout << a << "\n";
+	}
+}
