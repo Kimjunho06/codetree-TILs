bf9bdb31e69e9a9d96167e284b1e7ea9e58b8941
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2024-01-16 08:26:41 +0900

    Title: 특별한 문자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/특별한 문자/special-character.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+	string str;
+	unordered_map<char, int> mp;
+
+	cin >> str;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (mp.find(str[i]) != mp.end()) {
+			mp.find(str[i])->second++;
+		}
+		else {
+			mp.insert({ str[i], 1 });
+		}
+	}
+
+	char answer = ' ';
+	for (auto a : mp) {
+		if (a.second > 1) continue;
+
+		answer = a.first;
+		break;
+	}
+
+	if (answer == ' ')
+		cout << "None";
+	else
+		cout << answer;
+}
