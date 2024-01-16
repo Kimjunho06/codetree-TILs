6f13b6da2be1a484f705bccdfdc73b79850b6b52
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2024-01-16 10:45:57 +0900

    Title: 서로 다른 숫자, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/서로 다른 숫자/distinct-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+	int n;
+	unordered_set<int> st;
+
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+
+		st.insert(a);
+	}
+
+	cout << st.size();
+}
